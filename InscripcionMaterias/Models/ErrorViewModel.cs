451fe669b0191a29308ac67d7c1f8257a29b8d03
Fernando Javier <fernando.rodriguez@ansp.edu.sv>
2025-05-06 08:15:25 -0600

    Agregar archivos de proyecto.

---
+++ InscripcionMaterias/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InscripcionMaterias.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
