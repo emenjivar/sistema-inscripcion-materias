05e78e5c7c897b028e686f3851240e1f43b2be6e
Fernando Javier <fernando.rodriguez@ansp.edu.sv>
2025-06-08 04:05:35 -0600

    agregando opcion para cierre de ciclo academico

---
+++ InscripcionMaterias/Models/ViewModels/CierreCicloViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace InscripcionMaterias.Models.ViewModels
+{
+    public class CierreCicloViewModel
+    {
+        public int IdAlumno { get; set; }
+        public string NombreAlumno { get; set; } = string.Empty;
+
+        public List<MateriaCierreVM> Materias { get; set; } = new();
+    }
+
+    public class MateriaCierreVM
+    {
+        public int IdMateria { get; set; }
+        public string NombreMateria { get; set; } = string.Empty;
+        public bool Aprobado { get; set; }
+    }
+
+}
